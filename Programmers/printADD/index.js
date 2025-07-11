a456c793a5552ed9725b94a6f09a69cffd6dcb7d
y-minion <dudals2963@tukrorea.ac.kr>
2025-07-11 21:58:26 +0900

    주석

--- Programmers/printADD/index.js
+++ Programmers/printADD/index.js
@@ -1,15 +1,15 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-let input = [];
+// let input = [];
 
-rl.on("line", function (line) {
-  input = line.split(" ");
-}).on("close", function () {
-  const A = Number(input[0]);
-  const B = Number(input[1]);
-  console.log(`${A} + ${B} = ${A + B}`);
-});
+// rl.on("line", function (line) {
+//   input = line.split(" ");
+// }).on("close", function () {
+//   const A = Number(input[0]);
+//   const B = Number(input[1]);
+//   console.log(`${A} + ${B} = ${A + B}`);
+// });
